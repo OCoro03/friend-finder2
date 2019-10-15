11bca5f0e7c9462b818b44cc87aca96e723040d9
Orlando Corona <corona.orlando@gmail.com>
2019-10-15 00:11:20 -0500

    update

--- friends.js
+++ friends.js
@@ -3,16 +3,32 @@ var friends = [
         name: "Orlando",
         picture: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [
-            3,
-            4,
-            3,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Dave",
+        picture: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
             5,
-            4,
-            3,
-            4,
             5,
             5,
-            3
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 ];
--- server.js
+++ server.js
@@ -15,16 +15,32 @@ var friends = [
         name: "Orlando",
         picture: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores: [
-        3,
-        4,
-        3,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+        ]
+    },
+    {
+      name: "Sergio",
+        picture: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/2224243.jpg",
+        scores: [
         5,
-        4,
-        3,
-        4,
         5,
         5,
-        3
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
         ]
     }
 ];
@@ -48,6 +64,32 @@ app.get("/api/friends", function(req, res) {
     
     var newFriend = req.body;
     
+    //////////////
+    // for(var i = 0; i < newFriend.scores.length; i++) {
+    //   newFriend.scores[i] = parseInt(newFriend.scores[i]);
+    // }
+
+    // // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    // var bestFriendIndex = 0;
+    // var minimumDifference = 40;
+
+    // // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+    // //  whatever the difference is, add to the total difference
+    // for(var i = 0; i < friends.length; i++) {
+    //   var totalDifference = 0;
+    //   for(var j = 0; j < friends[i].scores.length; j++) {
+    //     var difference = Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+    //     totalDifference += difference;
+    //   }
+
+    //   // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+    //   if(totalDifference < minimumDifference) {
+    //     bestFriendIndex = i;
+    //     minimumDifference = totalDifference;
+    //   }
+    // }
+    //////////////
+
     newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
     
     friends.push(newFriend);
